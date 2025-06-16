bb3fe5c90af63872e1f6782b382572e6da85b4b8
Matías <matiasverailabaca@gmail.com>
2025-06-15 21:32:44 -0400

    Cambios

--- Calculadora/calculadoragrande.py
+++ Calculadora/calculadoragrande.py
@@ -16,46 +16,31 @@ while opcion < 5:
     if opcion == 1:
         print ("¡Suma!")
         num1 = int(input("Ingrese el primer número: "))
-        num2 = int(input("Ingrese el segundo número: "))
-        
+        num2 = int(input("Ingrese el segundo número: ")) 
         resultado = num1 + num2
-        
         print (f"El resultado de la suma es: {resultado}")
         opcion = int(input("Ingrese una opción: "))
     elif opcion == 2:
-        
         print ("¡Resta!")
-
         num1 = int(input("Ingrese el primer número: "))
         num2 = int(input("Ingrese el segundo número: "))
-        
         resultado = num1 - num2
-        
         print (f"El resultado de la resta es: {resultado}")
         opcion = int(input("Ingrese una opción: "))
     elif opcion == 3:
-        
         print ("¡Multiplicación!")
-
         num1 = int(input("Ingrese el primer número: "))
         num2 = int(input("Ingrese el segundo número: "))
-        
         resultado = num1 * num2
-        
         print (f"El resultado de la multiplicación es: {resultado}")
         opcion = int(input("Ingrese una opción: "))
     elif opcion == 4:
-        
         print ("¡División!")
-
         num1 = int(input("Ingrese el primer número: "))
         num2 = int(input("Ingrese el segundo número: "))
-        
         while num2 == 0:
             print ("¡No se puede dividir por cero!")
             num2 = int(input("Ingrese el divisor otra vez: "))
-        
         resultado = num1 / num2
-        
         print (f"El resultado de la división es: {resultado}")
         opcion = int(input("Ingrese una opción: "))
